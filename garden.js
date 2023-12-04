24ac97007ca32b3a2167b1d8706c898ee12c4b8d
Matt Wiese <matthewwiese@users.noreply.github.com>
2023-12-03 20:39:07 -0500

    Add info modal

--- garden.js
+++ garden.js
@@ -1,14 +1,29 @@
+function toggleModal(event) {
+    L.DomEvent.preventDefault(event);
+    const modal = document.getElementById('modal');
+    const style = getComputedStyle(modal);
+    if (style.display === 'flex') {
+        modal.style.display = 'none';
+    } else {
+        if (!('originalEvent' in event)) modal.style.display = 'flex';
+    }
+}
+
+document.querySelector('button#about').addEventListener("click", toggleModal);
+document.querySelector('div#modal > button').addEventListener("click", toggleModal);
+
 const map = L.map('map').setView([42.443333, -76.5], 12);
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
     { attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012' }
 ).addTo(map);
+
+map.on('click', toggleModal);
 
 const gardens = await (await fetch('./gardens.json')).json();
 for (const garden of gardens) {
     const socials = garden.socials.map(social => {
         return `<a href="${Object.values(social)[0]}">${Object.keys(social)[0]}</a>`
     }).join(' · ');
-    console.log(socials);
     L.marker([garden.lat, garden.long]).addTo(map).bindPopup(`<div><strong>${garden.name}</strong>
     ${garden.address}
     <p>${garden.description}</p>
